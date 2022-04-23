939bc51ab837dae034ec53716a1f8f78af8cfada
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-04-23 22:12:48 +0200

    Key event listener

---
+++ project/script.js
@@ -0,0 +1,14 @@
+const tagsElement = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10)
+        randomSelect();
+    }
+})

b1024b01a19a3e17f05146232f7f556dfc9306f8
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-04-23 22:13:41 +0200

    Random select Tag

--- project/script.js
+++ project/script.js
@@ -24,3 +24,32 @@ function createTags(input) {
         tagsElement.appendChild(tagElement);
     });
 }
+
+function randomSelect() {
+    const times = 30;
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unHighlightTag(randomTag);
+        }, 100)
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+
+            highlightTag(randomTag);
+        }, 100)
+
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)]
+}
